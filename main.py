87bf2963a545e2498493f6b0679e3a9274938a04
Antoine Pigeon <antoine_pigeon@hotmail.fr>
2026-07-12 17:12:33 -0400

    Add find objectID and wrap it in a function

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-URL = "https://www.metmuseum.org/art/collection/search/16584"
+def scrape_artwork(object_id):
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.9",
-}
+    URL = f"https://www.metmuseum.org/art/collection/search/{object_id}"
 
-response = requests.get(URL, headers=HEADERS, timeout=10)
-response.raise_for_status
-soup = BeautifulSoup(response.text, "html.parser")
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+    }
 
-data = {}
+    response = requests.get(URL, headers=HEADERS, timeout=10)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "html.parser")
 
-tombstone = soup.select_one("[class*='tombstone']")
-tombstone_ls = tombstone.select("li")
+    data = {}
 
-for li in tombstone_ls:
-    label = li.find("strong").get_text(strip=True).replace(":", "")
-    value = li.get_text(strip=True).removeprefix(label + ":")
-    data[label] = value
+# Get the overview info
+    tombstone = soup.select_one("[class*='tombstone']")
+    tombstone_ls = tombstone.select("li")
+    for li in tombstone_ls:
+        label = li.find("strong").get_text(strip=True).replace(":", "")
+        value = li.get_text(strip=True).removeprefix(label + ":")
+        data[label] = value
 
-print(data)
+# Get the object ID
+    script = soup.find("script", id="analytics")
+    text = script.get_text()
+    analytics_data = json.loads(text)
+    object_id_value = analytics_data["objectID"]
+    data["objectID"] = object_id_value
+
+    return data
+
+print(scrape_artwork(16584))
